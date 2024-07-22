a36ac52bde01aae3a33695ef3cfc86e40c3c6ef8
Matt Mazer <mazermatt3@gmail.com>
2024-07-22 23:56:50 +1000

    wip

--- src/db/db.go
+++ src/db/db.go
@@ -3,28 +3,34 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func Connect() {
-	testing := os.Getenv("DB_CONNECTION_URL")
-	fmt.Println("testing babyyyyyy")
-	fmt.Println(testing)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// password := os.Getenv("PASSWORD")
 	// if password == "" {
 	// 	log.Fatal("PASSWORD environment variable not set")
 	// }
-	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_CONNECTION_URL"))
+	// dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_CONNECTION_URL"))
+	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	dbpool, err := pgx.Connect(context.Background(), os.Getenv("DB_CONNECTION_URL"))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer dbpool.Close()
+	defer dbpool.Close(context.Background())
 
 	var greeting string
-	err = dbpool.QueryRow(context.Background(), "select * from UserVisitCount").Scan(&greeting)
+	err = dbpool.QueryRow(context.Background(), "SELECT * FROM uservisitcount").Scan(&greeting)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
