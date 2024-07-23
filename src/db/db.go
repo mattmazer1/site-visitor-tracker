52aad8b89ea516aa56f6175d4474fb8ef51117a7
Matt Mazer <mazermatt3@gmail.com>
2024-07-23 23:54:34 +1000

    wip

--- src/db/db.go
+++ src/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,32 +11,43 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func Connect() {
+func Connect() error {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// password := os.Getenv("PASSWORD")
-	// if password == "" {
-	// 	log.Fatal("PASSWORD environment variable not set")
-	// }
-	// dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_CONNECTION_URL"))
-	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	dbpool, err := pgx.Connect(context.Background(), os.Getenv("DB_CONNECTION_URL"))
+	password := os.Getenv("DATABASE_URL")
+	if password == "" {
+		log.Fatal("PASSWORD environment variable not set")
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer dbpool.Close(context.Background())
+	defer conn.Close(context.Background())
 
-	var greeting string
-	err = dbpool.QueryRow(context.Background(), "SELECT * FROM uservisitcount").Scan(&greeting)
+	// var test int
+	// err = conn.QueryRow(context.Background(), "select * from uservisitcount").Scan(&test)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	commandTag, err := conn.Exec(context.Background(), "insert into uservisitcount (count) values ($1)", 5) //fetch and update in same query
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
+		return err
 	}
 
-	fmt.Println(greeting)
-	fmt.Println("Connecting to db..")
+	if commandTag.RowsAffected() != 1 {
+		return errors.New("no row found to delete")
+	}
+	fmt.Println(commandTag)
+
+	return nil
 }
+
+// UPDATE uservisitcount
+// SET count = count + 1
