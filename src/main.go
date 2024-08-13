573f590d1b338ecafca9deea934d6339ff3d34ca
Matt Mazer <mazermatt3@gmail.com>
2024-08-13 16:52:56 +1000

    wip

--- src/main.go
+++ src/main.go
@@ -14,6 +14,21 @@ type UserIP struct {
 	IP string `json:"ip"`
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func GetUserData(w http.ResponseWriter, r *http.Request) {
 	userData, err := db.GetUserData()
 	if err != nil {
@@ -65,9 +80,13 @@ func main() {
 	db.Connect()
 	defer db.CloseDb()
 
-	http.HandleFunc("GET /user-data", GetUserData)
-	http.HandleFunc("POST /add-visit", AddUserData)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /user-data", GetUserData)
+	mux.HandleFunc("POST /add-visit", AddUserData)
+
+	handler := corsMiddleware(mux)
 
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
