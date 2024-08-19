6e86813890c89ed33032049398a20b50a9119e4e
Matt Mazer <mazermatt3@gmail.com>
2024-08-19 22:46:38 +1000

    wip

--- Terraform/Networking/vpc.tf
+++ Terraform/Networking/vpc.tf
@@ -1,6 +1,30 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/28"
   instance_tenancy = "default"
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+
+#Routes
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "example"
+  }
 }
 
 # TODO
